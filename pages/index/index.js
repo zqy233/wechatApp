4426e26fc610ada55839b4ecac0c2ce86cc6b0ed
wxzqy <wxzqy@yonyou.com>
2021-02-11 01:26:48 +0800

    wechatApp

---
+++ pages/index/index.js
@@ -0,0 +1,41 @@
+//Page Object
+Page({
+  data: {
+    msg:'Hello',
+    number:100,
+    isGirl:false
+  },
+  //options(Object)
+  onLoad: function(options) {
+    
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
+  }
+});
+  
